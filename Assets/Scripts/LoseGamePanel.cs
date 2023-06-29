9b6f5c2986092f500d096ab7b90d391a25ac4db1
kimduc14092003 <100016483+kimduc14092003@users.noreply.github.com>
2023-06-29 15:34:58 +0700

    Change All sprite to Atlas without lag

--- Assets/Scripts/LoseGamePanel.cs
+++ Assets/Scripts/LoseGamePanel.cs
@@ -6,10 +6,9 @@ using UnityEngine;
 public class LoseGamePanel : MonoBehaviour
 {
     public GameObject loseLight;
-
     private void Start()
     {
-        loseLight.transform.DOLocalRotate(new Vector3(0, 0, 360), 60, RotateMode.FastBeyond360)
+        loseLight.transform.DOLocalRotate(new Vector3(0, 0, -360), 60, RotateMode.FastBeyond360)
                                 .SetEase(Ease.Linear)
                                 .SetLoops(-1);
     }
